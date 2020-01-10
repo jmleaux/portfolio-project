174f546f2543928bee165e6ecf5432aa93d5ef9c
J. M. Lowe <chip@jmlowe.net>
2020-01-10 12:53:52 +0300

    finished job tiles

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# title, publication date, body, image
+
+class Blog(models.Model):
+    title = models.CharField(max_length=128)
+    pub_date = models.DateTimeField(auto_now=True)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    def __str__(self):
+        return(self.title)
