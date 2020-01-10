47e77d065e92a5872eba3f3edfe8b2ee2b237b1a
J. M. Lowe <chip@jmlowe.net>
2020-01-10 17:58:19 +0300

    changed postgres password

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': '=Be1j1ng!',
+        'PASSWORD': 'PVDeU?6rifPMGj',
         'HOST': 'localhost',
         'PORT': 5432,
     }
